01080ba4ac9f40bd0e3ce1bd5259dabb6f308e52
Sean Meininger <sean.meininger@pm.me>
2021-01-30 02:31:29 -0800

    idk still

--- bin/cdk-project.ts
+++ bin/cdk-project.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
-import { CdkProjectStack } from '../lib/cdk-project-stack';
+import { CdkWorkshopStack } from '../lib/cdk-project-stack';
 
 const app = new cdk.App();
-new CdkProjectStack(app, 'CdkProjectStack');
+new CdkWorkshopStack(app, 'CdkProjectStack');
--- lib/cdk-project-stack.ts
+++ lib/cdk-project-stack.ts
@@ -1,15 +1,15 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 
-export class CdkProjectStack extends cdk.Stack {
+export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // defines an AWS Lambda resource
     const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_10_X, //execution environment
-      code: lambda.Code.fromAsset('lambda'),// load from 'lambda' directory
-      handler: 'hello.handler' //file is "hello" function is "handler"
+      runtime: lambda.Runtime.NODEJS_10_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
     });
-   
   }
 }
