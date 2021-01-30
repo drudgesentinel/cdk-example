8d490882ded800b829124c38abe18a2b0068bb20
Sean Meininger <sean.meininger@pm.me>
2021-01-30 01:28:35 -0800

    removed defaults

--- lib/cdk-project-stack.ts
+++ lib/cdk-project-stack.ts
@@ -1,9 +1,12 @@
+import * as sns from '@aws-cdk/aws-sns';
+import * as subs from '@aws-cdk/aws-sns-subscriptions';
+import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 
 export class CdkProjectStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+   
   }
 }
